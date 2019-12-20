8a7129bbbf9847bcae6a72a3039b1fe2432a874e
austinbillings <austin@austinbillings.com>
2019-12-20 06:32:04 -0500

    move error boundary into folder, add sass

--- components/tools/error-boundary/error-boundary.jsx
+++ components/tools/error-boundary/error-boundary.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 
-import { Inspector } from './inspector';
+import './error-boundary.scss';
+import { Inspector } from '../inspector';
 
 export class ErrorBoundary extends React.Component {
     constructor (props) {
@@ -24,24 +25,28 @@ export class ErrorBoundary extends React.Component {
         const displayableError = caughtError || error;
 
         return !displayableError ? children : (
-            <div className="border-danger pad-30 stack" style={{ borderWidth: '5px', height: '100vh', flexWrap: 'nowrap', overflow: 'auto' }}>
-                <p className="text-engraved text-danger margin-0">
-                    Error encountered
+            <div className="border-danger pad-30 stack error-boundary-wrapper">
+                <p className="text-engraved margin-0">
+                    Exception
                 </p>
 
-                {displayableError.message && <h2 className="text-italic">{displayableError.message}</h2>}
+                {displayableError.message && (
+                    <h2 className="text-italic border-color-danger error-boundary-header">
+                        {displayableError.message}
+                    </h2>
+                )}
 
-                <h6 className="text-subtle">Location</h6>
+                <h6 className="text-subtle margin-top-20 margin-bottom-0">Location</h6>
                 <h5>{displayableError.fileName} <br/> Line {displayableError.lineNumber}, column {displayableError.columnNumber}</h5>
 
                 {displayableError.contextData && (
                     <Fragment>
-                        <h6 className="text-subtle">Context data</h6>
+                        <h6 className="text-subtle margin-top-20 margin-bottom-0">Context data</h6>
                         <Inspector data={displayableError.contextData}/>
                     </Fragment>
                 )}
 
-                <h6 className="text-subtle">Stack trace</h6>
+                <h6 className="text-subtle margin-top-20 margin-bottom-0">Stack trace</h6>
                 <small>
                     <blockquote className="border-color-warning">
                         <code>{displayableError.stack}</code>
--- components/tools/index.js
+++ components/tools/index.js
@@ -1,6 +1,6 @@
 export { Inspector } from './inspector';
 export { Portal } from './portal';
 export { Specimen } from './specimen';
-export { ErrorBoundary } from './error-boundary';
 export { ModalBody } from './modal-body/modal-body';
 export { ModalWrapper } from './modal-wrapper/modal-wrapper';
+export { ErrorBoundary } from './error-boundary/error-boundary';
