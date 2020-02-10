9ac43116ae9595da3bdbb27c1e63d222eb019cd3
austinbillings <austin@austinbillings.com>
2020-02-10 04:39:53 -0500

    tools: fix ScrollWatcher export

--- tools/index.js
+++ tools/index.js
@@ -1,7 +1,7 @@
 export { Inspector } from './inspector';
 export { Portal } from './portal';
 export { Specimen } from './specimen';
+export { ScrollWatcher } from './scroll-watcher';
 export { ModalBody } from './modal-body/modal-body';
 export { ModalWrapper } from './modal-wrapper/modal-wrapper';
 export { ErrorBoundary } from './error-boundary/error-boundary';
-export { ScrollWatcher } from './scroll-watcher/scroll-watcher';
