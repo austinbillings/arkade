c214436736b363b17a954bbea19c786d8f38c149
austinbillings <austin@austinbillings.com>
2020-04-17 01:05:40 -0400

    forms/input: add select/dropdown, longtext/textarea/paragraph, checkbox support, add [placeholder]

--- forms/input/input.jsx
+++ forms/input/input.jsx
@@ -3,12 +3,53 @@ import React from 'react';
 import './input.scss';
 import { isFunction } from 'arkade/utils/type-utils';
 
-export const Input = ({ type, value, onChange, className, ...rest }) => {
+export const Input = ({ type, value, onChange, placeholder, className, options = [], ...rest }) => {
     const handleChange = isFunction(onChange)
         ? (newValue) => onChange(newValue)
         : (newValue) => null;
 
     switch (type) {
+        case 'select':
+        case 'dropdown':
+            return (
+                <select
+                    value={value || ''}
+                    className={`ak-select ${className}`}
+                    onChange={({ target }) => onChange(target.value)}>
+                    {options.map(({ name, value }, index) => (
+                        <option key={index} value={value}>{name}</option>
+                    ))}
+                </select>
+            );
+        case 'longtext':
+        case 'textarea':
+        case 'paragraph':
+            return (
+                <textarea
+                    type={type || 'text'}
+                    className={`ak-input ${className}`}
+                    placeholder={placeholder || ''}
+                    onChange={({ target }) => onChange(target.value)}
+                    value={value}
+                    {...rest}
+                />
+            )
+        case 'checkbox':
+            return (
+                <div className={`ak-checkbox-group`}>
+                    <input
+                        checked={!!value}
+                        value={!!value}
+                        type="checkbox"
+                        className={`ak-checkbox-input ${className}`}
+                        onChange={({ target }) => onChange(target.value)}
+                        {...rest}
+                    />
+                    <p className="ak-checkbox-description" onClick={() => onChange(!value)}>
+                        {placeholder}
+                    </p>
+                </div>
+            )
         case 'password':
         case 'email':
         case 'text':
@@ -17,6 +58,7 @@ export const Input = ({ type, value, onChange, className, ...rest }) => {
                 <input
                     value={value}
                     type={type || 'text'}
+                    placeholder={placeholder || ''}
                     className={`ak-input ${className}`}
                     onChange={({ target }) => onChange(target.value)}
                     {...rest}
