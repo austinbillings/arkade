3a78dcb6c32d8abd4bd497a22341ac322466a38b
austinbillings <austin@austinbillings.com>
2020-05-18 00:30:56 -0400

    portal: make nextjs-compatible

--- tools/portal.jsx
+++ tools/portal.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 import { createPortal } from 'react-dom';
 
 export const Portal = ({ children }) => {
+    if (!process.browser) {
+        return React.Fragment;
+    }
+    
     const rootTarget = document.getElementById('modal-root');
 
     return createPortal(children, rootTarget);
