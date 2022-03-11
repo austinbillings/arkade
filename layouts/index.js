cc02ec0ac6dc5729fa96cbaf9c0a8b6855730ffe
austin <austin@austinbillings.com>
2022-03-11 00:56:13 -0500

    add <SwitchLayout>

--- layouts/index.js
+++ layouts/index.js
@@ -1,3 +1,4 @@
 export { RowLayout } from './row-layout';
 export { StackLayout } from './stack-layout';
 export { CentralLayout } from './central-layout';
+export { SwitchLayout } from './switch-layout';
