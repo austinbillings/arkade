a6185726612315d65c21c2c7df3f1c39f5883c9f
austin <austin@austinbillings.com>
2022-03-13 11:28:42 -0400

    add URL preference to <SocialIcon>

--- common/social-icon.jsx
+++ common/social-icon.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import { Icon } from './icon'
 import { getNetworkIcon, getNetworkUrlPrefix } from '../utils/social-utils'
 
-export const SocialIcon = ({ networkName, id }) => {
+export const SocialIcon = ({ networkName, id, url }) => {
     const icon = getNetworkIcon(networkName)
     const prefix = getNetworkUrlPrefix(networkName)
 
     const linkProps = {
         className: 'ak-social-icon',
         target: '_blank',
-        href: prefix + id,
+        href: url ? url : prefix + id,
         name: `${id} via ${networkName}`
     }
 
